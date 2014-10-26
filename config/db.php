e33a6a89201c1ef037849e6f06966fc74f0e12e8
Bowen <xiaoyoustudy@sohu.com>
2014-10-26 22:39:43 +0800

    Add Yii2 Framework
    Add Yii2 Framework. This website is developed using Yii2.

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'username' => 'root',
+    'password' => 'xiaowei1988620',
+    'charset' => 'utf8',
+];
