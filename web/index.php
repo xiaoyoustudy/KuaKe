b28dd7321af06c3de80629781e546b8a5ff0b457
Bowen <xiaoyoustudy@sohu.com>
2015-02-15 08:44:15 +0800

    Commit in Mac
    commit in mac

--- web/index.php
+++ web/index.php
@@ -4,9 +4,14 @@
 defined('YII_DEBUG') or define('YII_DEBUG', true);
 defined('YII_ENV') or define('YII_ENV', 'dev');
 
+// register Composer autoloader
 require(__DIR__ . '/../vendor/autoload.php');
+
+// include Yii class file 
 require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
 
+// load application configuration
 $config = require(__DIR__ . '/../config/web.php');
 
+// create, configure and run application
 (new yii\web\Application($config))->run();
